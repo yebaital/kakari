5719fe6db216b795515c9d938568a9d12d0c3299
Yousef <yousef.baitalmal@gmail.com>
2024-03-07 21:21:57 +0300

    Add detailed docstrings to task services and API handlers
    The commit adds extensive docstrings to the functions under task services and API handlers. The docstrings provide an explanation of these functions including their parameters, return values, and possible exceptions.

--- backend/app/api/v1/handlers/task.py
+++ backend/app/api/v1/handlers/task.py
@@ -4,7 +4,6 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException
 
 from app.api.deps.user_deps import get_current_user
-from app.models.task_model import Task
 from app.models.user_model import User
 from app.schemas.task_schema import TaskOut, TaskCreate, TaskUpdate
 from app.services.task_service import TaskService
@@ -14,21 +13,58 @@ task_router = APIRouter()
 
 @task_router.get('/', summary="Get all tasks of the user", response_model=List[TaskOut])
 async def list_user_tasks(current_user: User = Depends(get_current_user)):
+    """
+    Get all tasks of the user
+
+    :param current_user: The current user
+    :return: A list of tasks for the user
+    :rtype: List[TaskOut]
+    """
     return await TaskService.list_tasks(current_user)
 
 
 @task_router.post('create', summary="Create a new task", response_model=TaskOut)
 async def create_task(data: TaskCreate, current_user: User = Depends(get_current_user)):
+    """
+    Create a new task.
+
+    :param data: The data required to create the task.
+    :type data: TaskCreate
+
+    :param current_user: The current user who is creating the task.
+    :type current_user: User
+
+    :return: The newly created task.
+    :rtype: TaskOut
+    """
     return await TaskService.create_task(data=data, user=current_user)
 
 
 @task_router.get('/{task_id}', summary="Get a task by id", response_model=TaskOut)
 async def get_task_by_id(task_id: UUID):
+    """
+       Get a task by id.
+
+       :param task_id: The id of the task to retrieve.
+       :type task_id: :class:`UUID`
+
+       :return: The task with the specified id.
+       :rtype: :class:`TaskOut`
+
+    """
     return await TaskService.get_task_by_id(task_id)
 
 
 @task_router.put('/{task_id}', summary="Update task by id", response_model=TaskOut)
 async def update_task(task_id: UUID, data: TaskUpdate, current_user: User = Depends(get_current_user)):
+    """
+    Update a task based on task_id.
+
+    :param task_id: The ID of the task to update.
+    :param data: The updated task data.
+    :param current_user: The current user making the request.
+    :return: The updated task.
+    """
     task = await TaskService.get_task_by_id(task_id)
 
     if not task:
@@ -43,5 +79,12 @@ async def update_task(task_id: UUID, data: TaskUpdate, current_user: User = Depe
 
 @task_router.delete('/{task_id}', summary="Delete task by id")
 async def delete_task(task_id: UUID):
+    """
+    Delete task by id.
+
+    :param task_id: The ID of the task to be deleted.
+    :return: None
+
+    """
     await TaskService.delete_task(task_id=task_id)
     return None
--- backend/app/services/task_service.py
+++ backend/app/services/task_service.py
@@ -11,21 +11,50 @@ from app.schemas.task_schema import TaskCreate, TaskUpdate
 class TaskService:
     @staticmethod
     async def list_tasks(user: User) -> List[Task]:
+        """
+        List tasks created by a user.
+
+        :param user: The user for whom to list tasks.
+        :return: A list of tasks created by the user.
+        :rtype: list
+        """
         tasks = await Task.find(Task.task_creator.id == user.id).to_list()
         return tasks
 
     @staticmethod
     async def create_task(user: User, data: TaskCreate) -> Task:
+        """
+        Create a new task.
+
+        :param user: The user who is creating the task.
+        :param data: The data for creating the task (TaskCreate object).
+        :return: The created task (Task object).
+        """
         task = Task(**data.dict(exclude_unset=True), task_creator=user)
         return await task.insert()
 
     @classmethod
     async def get_task_by_id(cls, task_id: UUID):
+        """
+        Retrieves a task by its ID.
+
+        :param task_id: The ID of the task to retrieve.
+        :return: The task object if found, None otherwise.
+        """
         task = await Task.find_one(Task.task_id == task_id)
         return task
 
     @classmethod
     async def update_task(cls, task_id: UUID, data: TaskUpdate) -> Task:
+        """
+        Updates a task with the given task_id using the provided data.
+
+        :param task_id: The UUID of the task to be updated.
+        :param data: The TaskUpdate object containing the updated data for the task.
+        :return: The updated Task object.
+        :raises ValueError: If an invalid task_id is provided.
+        :raises HTTPException: If the task is not found.
+        """
         try:
             task_id
         except ValueError:
@@ -43,6 +72,12 @@ class TaskService:
 
     @staticmethod
     async def delete_task(task_id: UUID):
+        """
+        Delete a task by task ID.
+
+        :param task_id: The ID of the task to be deleted.
+        :return: None.
+        """
         task = await TaskService.get_task_by_id(task_id=task_id)
         if task:
             await task.delete()
