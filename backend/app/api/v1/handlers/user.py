3948543cfaa18b1d06d79f1ab6ce9463e0b5ffb8
Yousef <yousef.baitalmal@gmail.com>
2024-03-09 03:22:01 +0300

    Refactor user update functionality with role-based authorization
    Implemented role-based authorization for user update features. Altered the `update_user_by_id` function to check user's authorization before updating details. Added a new function `update_user_roles` which allows only admin users to modify user roles. Corresponding API handlers were adjusted to reflect these changes.

--- backend/app/api/v1/handlers/user.py
+++ backend/app/api/v1/handlers/user.py
@@ -1,21 +1,71 @@
-import pymongo.errors
-from fastapi import APIRouter, HTTPException, status
+from typing import List
+from uuid import UUID
 
-import app.schemas.user_schema
+import pymongo.errors
+from fastapi import APIRouter, HTTPException, status, Depends
+
+from app.api.deps.user_deps import get_current_user
 from app.schemas.user_schema import UserAuth
+from app.schemas.user_schema import UserOut, UserUpdate
 from app.services.user_service import UserService
+from app.utils.utils import validate_uuid
 
 user_router = APIRouter()
 
 
-@user_router.post('create', summary="Create new user", response_model=app.schemas.user_schema.UserOut)
+# TODO: add endpoint to update user roles for admins only
+# TODO: add endpoint to update user password
+
+@user_router.post('/create', summary="Create new user", response_model=UserOut)
 async def create_user(data: UserAuth):
+    """
+    Create new user.
+
+    Args:
+        data (UserAuth): The user authentication data.
+
+    Returns:
+        UserOut: The created user data.
+
+    Raises:
+        HTTPException: If the user with the given email or username already exists.
+    """
     try:
         return await UserService.create_user(data)
     except pymongo.errors.DuplicateKeyError:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="User with this email or username already exists")
 
-    # TODO: add endpoint to update user
-    # TODO: add endpoint to update user roles for admins only
-    # TODO: add endpoint to update user password
+
+@user_router.put('/', summary="Update user", response_model=UserOut)
+async def update_user(user_id: UUID, data: UserUpdate):
+    """
+    Update user.
+
+    Parameters:
+    - user_id (UUID): The ID of the user to update.
+    - data (UserUpdate): The data with which to update the user.
+
+    Returns:
+    - UserOut: The updated user.
+    """
+    validate_uuid(user_id)
+    return await UserService.update_user_by_id(user_id=user_id, user_update=data)
+
+
+@user_router.put('/update-roles/{user_id}', summary="Update user roles", response_model=UserOut)
+async def update_user_roles(user_id: UUID, roles: List[str], current_user: Depends(get_current_user)):
+    """
+    Update User Roles
+
+    Update the roles of a user identified by their user ID.
+
+    Parameters:
+    - user_id (UUID): The unique identifier of the user.
+    - roles (List[str]): A list of role names to be assigned to the user.
+    - current_user: The currently authenticated user making the request.
+
+    Returns:
+    - UserOut: The updated user object with the modified roles.
+    """
+    return await UserService.update_user_roles(user_id=user_id, user_roles=roles, current_user=current_user)
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,11 +1,12 @@
-from typing import Optional
+from typing import Optional, List
 from uuid import UUID
 
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
 
 from app.core.security import get_password, verify_password
 from app.models.user_model import User
 from app.schemas.user_schema import UserAuth, UserUpdate
+from ..api.deps.user_deps import get_current_user
 from ..utils.utils import validate_uuid
 
 
@@ -71,23 +72,63 @@ class UserService:
         return user
 
     @classmethod
-    async def update_user_by_id(cls, user_id: UUID, user_update: UserUpdate) -> User:
+    async def update_user_by_id(cls, user_id: UUID, user_update: UserUpdate,
+                                current_user=Depends(get_current_user)) -> User:
         """
-        Update user by ID.
+        Updates a user by their ID.
 
-        :param user_id: The ID of the user to update.
-        :param user_update: The updated user information.
-        :return: The updated user object.
+        Parameters:
+        - user_id: UUID - The ID of the user to update.
+        - user_update: UserUpdate - The updated user data to apply.
+        - current_user (optional): Depends - The current user making the request. Defaults to the result of the `get_current_user` function.
+
+        Returns:
+        - User - The updated user object.
+
+        Raises:
+        - HTTPException - If the user with the given ID is not found or if the current user is unauthorized.
         """
         validate_uuid(user_id)
         user = await cls.get_user_by_id(user_id=user_id)
         if not user:
-            raise HTTPException(status_code=404, detail="Task not found")
+            raise HTTPException(status_code=404, detail="User not found")
 
-        await user.update({"$set": user_update.dict(exclude_unset=True)})
-        await user.save()
+        if user is current_user or "admin" in current_user.roles:
+            await user.update({"$set": user_update.dict(exclude_unset=True)})
+            await user.save()
+        else:
+            raise HTTPException(status_code=401, detail="Unauthorized")
 
         return user
 
     # TODO: add method to update user password
-    # TODO: add method to update user roles, only for admins
+    @classmethod
+    async def update_user_roles(cls, user_id: UUID, current_user: Depends(get_current_user),
+                                user_roles: List[str]) -> User:
+        """
+            Update User Roles method.
+
+            Parameters:
+            - user_id (UUID): The UUID of the user to update.
+            - current_user (Depends(get_current_user)): The current authenticated user.
+            - user_roles (List[str]): The new roles to assign to the user.
+
+            Returns:
+            - User: The updated user instance.
+
+            Raises:
+            - HTTPException - 404: If the user is not found.
+            - HTTPException - 401: If the current user does not have admin privileges.
+        """
+        user = await cls.get_user_by_id(user_id=user_id)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        if "admin" in current_user.roles:
+            user.roles = user_roles
+            await user.save()
+
+        else:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+        return user
