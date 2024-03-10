5885cf5e60c028743ff67f2f8ca44798a595b303
yebaital <yousef.baitalmal@gmail.com>
2024-03-10 12:59:57 +0300

    Add project endpoints
    A series of project management endpoints have been added to the router.py in the API V1 route, replacing a series of TODOs. This includes project creation, deletion, updating, listing, and endpoints for listing project tasks and members.

--- backend/app/api/v1/handlers/project.py
+++ backend/app/api/v1/handlers/project.py
@@ -1,6 +1,51 @@
-# TODO: add endpoint to list project members
-# TODO: add endpoint to list project tasks
-# TODO: add endpoint to create project
-# TODO: add endpoint to delete project
-# TODO: add endpoint to update project
-# TODO: add endpoint to list projects
+from typing import List
+from uuid import UUID
+
+from beanie import Link
+from fastapi import APIRouter, Depends
+
+from app.api.deps.user_deps import get_current_user
+from app.models.project_model import Project
+from app.models.task_model import Task
+from app.models.user_model import User
+from app.schemas.project_schema import ProjectCreate, ProjectUpdate
+from app.services.project_service import ProjectService
+
+project_router = APIRouter()
+
+
+@project_router.get('/', summary="Get all projects")
+async def get_all_projects() -> List[Project]:
+    return await ProjectService.list_projects()
+
+
+@project_router.get('/{project_id}', summary="Get a single project by id")
+async def get_project_by_id(project_id: UUID) -> Project:
+    return await ProjectService.get_project_by_id(project_id)
+
+
+@project_router.post('/', summary="Create a new project")
+async def create_project(project: ProjectCreate, current_user: User = Depends(get_current_user)) -> Project:
+    return await ProjectService.create_project(project, current_user)
+
+
+@project_router.put('/{project_id}', summary="Update project by id")
+async def update_project(project_id: UUID, project: ProjectUpdate, current_user: User = Depends(get_current_user)):
+    return await ProjectService.update_project(project_id, project, current_user)
+
+
+@project_router.delete('/{project_id}', summary="Delete project by id")
+async def delete_project(project_id: UUID, current_user: User = Depends(get_current_user)):
+    return await ProjectService.delete_project(project_id, current_user)
+
+
+@project_router.get('/tasks/{project_id}', summary="Get all tasks for a project")
+async def get_project_tasks(project_id: UUID) -> List[Link[Task]]:
+    project = await ProjectService.get_project_by_id(project_id)
+    return project.tasks
+
+
+@project_router.get('/members/{project_id}', summary="Get all project members")
+async def get_project_members(project_id: UUID) -> List[Link[User]]:
+    project = await ProjectService.get_project_by_id(project_id)
+    return project.project_members
--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.auth.jwt import auth_router
-from app.api.v1.handlers import user, task
+from app.api.v1.handlers import user, task, project
 
 router = APIRouter()
 router.include_router(user.user_router, prefix="/user", tags=["user"])
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
 router.include_router(task.task_router, prefix="/task", tags=["task"])
-
-# TODO: add project router
+router.include_router(project.project_router, prefix="/project", tags=["project"])
