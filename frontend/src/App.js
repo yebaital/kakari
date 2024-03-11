f760d0617b7c930d135c65501faca4794f9cab4c
Yousef <yousef.baitalmal@gmail.com>
2024-03-12 01:59:14 +0300

    Update frontend by deleting non-essential files
    Removed App.css, App.test.js, logo.svg, and package-lock.json files from the frontend directory. This cleanup serves to reduce unnecessary complexity in the project structure.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Router>
+        <Routes>
+            <Route path="/login" element={<h1>Login</h1>} />
+            <Route path="/register" element={<h1>Register</h1>} />
+        </Routes>
+    </Router>;
 }
 
 export default App;
